f82f67f49b625a22aa6c5c20a3b40a1a962a5965
Slapec <slapec@gmail.com>
2017-08-01 00:18:28 +0200

    Removed the index view

--- wimm/api.py
+++ wimm/api.py
@@ -1,5 +1,20 @@
 # coding: utf-8
 
+from django.conf import settings
+from django.middleware import csrf
+
 from rest_framework import routers
+from rest_framework.response import Response
+from rest_framework.viewsets import ViewSet
 
 api = routers.DefaultRouter()
+
+
+class SettingsView(ViewSet):
+    def list(self, request):
+        return Response({
+            'csrftoken': csrf.get_token(request),
+            'root': settings.FORCE_SCRIPT_NAME or ''
+        })
+
+api.register(r'settings', SettingsView, 'settings')
--- wimm/urls.py
+++ wimm/urls.py
@@ -3,9 +3,8 @@
 from django.conf.urls import url, include
 
 from wimm.api import api
-from item.views import Index
+
 
 urlpatterns = [
-    url(r'^$', Index.as_view(), name='index'),
     url(r'api/', include(api.urls, namespace='api'))
 ]
